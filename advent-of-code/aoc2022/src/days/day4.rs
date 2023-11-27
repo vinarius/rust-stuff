3cab751af8307d9b5c25cc9748c7386562c801b5
Mark Kraus <vindevccm@gmail.com>
2023-11-27 16:45:42 -0500

    progress

--- advent-of-code/aoc2022/src/days/day4.rs
+++ advent-of-code/aoc2022/src/days/day4.rs
@@ -51,10 +51,46 @@ fn part1(input: &str) -> i32 {
     assignment_pairs_fully_contained
 }
 
-fn part2(_input: &str) -> i32 {
+fn part2(input: &str) -> i32 {
     println!("Running day 4 part 2");
 
-    todo!();
+    let mut assignment_pairs_partially_overlapping = 0;
+    const ERROR: &str = "problem statement guarantees a min and max range separated by a -";
+
+    for line in input.lines() {
+                let (range_one, range_two) = line
+            .split_once(',')
+            .expect("problem statement guarantees two ranges separated by a comma");
+
+        // println!("range_one: {}", range_one);
+        // println!("range_two: {}", range_two);
+
+        let (min_one, max_one) = range_one.split_once('-').expect(ERROR);
+
+        // println!("min_one: {min_one}");
+        // println!("max_one: {max_one}");
+
+        let (min_two, max_two) = range_two.split_once('-').expect(ERROR);
+
+        // println!("min_two: {min_two}");
+        // println!("max_two: {max_two}");
+
+        let min_one: i32 = min_one.parse().expect("min_one should be a number");
+        let max_one: i32 = max_one.parse().expect("max_one should be a number");
+        let min_two: i32 = min_two.parse().expect("min_two should be a number");
+        let max_two: i32 = max_two.parse().expect("max_two should be a number");
+        let is_partially_contained = (max_one >= min_two && min_one <= max_two) || (max_two >= min_one && max_two <= min_one);
+
+        // println!("is_fully_contained: {is_fully_contained}");
+
+        if is_partially_contained {
+            assignment_pairs_partially_overlapping += 1;
+        }
+    }
+
+    println!("assignment_pairs_partially_overlapping: {assignment_pairs_partially_overlapping}");
+
+    assignment_pairs_partially_overlapping
 }
 
 #[cfg(test)]
