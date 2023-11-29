21b62dbd824771ccc42b3251c8dd95e674caaea4
Mark Kraus <vindevccm@gmail.com>
2023-11-29 17:05:16 -0500

    progress

--- advent-of-code/aoc2022/src/days/day5.rs
+++ advent-of-code/aoc2022/src/days/day5.rs
@@ -11,28 +11,38 @@ pub fn run() {
 fn part1(input: &str) {
     println!("Running day 5 part 1");
 
-    println!("input:");
+    // println!("input: {input}");
 
     const ERROR: &str = "problem statement guarantees input structure";
     let mut supply_crates: Vec<&str> = Vec::new();
     let mut instructions: Vec<[i32; 3]> = Vec::new();
 
     for line in input.lines() {
+        // println!("line: {line}");
+
         if !line.starts_with("move") && !line.trim().is_empty() {
             supply_crates.push(line);
-        } else {
+        } else if line.trim().is_empty() {
+            continue;
+        }
+        else {
             let sanitized_instructions: Vec<i32> = Vec::new();
             let split_by_whitespace: Vec<&str> = line.split_whitespace().collect();
+
+            println!("split_by_whitespace: {split_by_whitespace:?}");
+
             let quantity: i32 = split_by_whitespace
                 .get(1)
                 .expect(ERROR)
                 .parse()
                 .expect(ERROR);
+
             let start: i32 = split_by_whitespace
                 .get(3)
                 .expect(ERROR)
                 .parse()
                 .expect(ERROR);
+
             let end: i32 = split_by_whitespace
                 .get(5)
                 .expect(ERROR)
@@ -40,14 +50,21 @@ fn part1(input: &str) {
                 .expect(ERROR);
 
             instructions.push([quantity, start, end]);
+
+            break;
         }
     }
 
-    dbg!("supply_crates: {supply_crates}");
+    println!("supply_crates: {supply_crates:#?}");
+    println!("instructions: {instructions:?}");
 }
 
 fn part2(input: &str) {
     println!("Running day 5 part 2");
 
     todo!();
+}
+
+struct CrateColumn {
+    
 }
